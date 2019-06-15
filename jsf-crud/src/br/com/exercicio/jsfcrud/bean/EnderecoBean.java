b0e9f4cba975abe97a7b9c1b8370a36c9aa4f4cc
Vanjhony Cosmo <jhonycosmo@gmail.com>
2019-06-15 17:04:14 -0300

    Adicionando filtro de uf->cidade no cadastro de endereco

--- jsf-crud/src/br/com/exercicio/jsfcrud/bean/EnderecoBean.java
+++ jsf-crud/src/br/com/exercicio/jsfcrud/bean/EnderecoBean.java
@@ -1,8 +1,12 @@
 package br.com.exercicio.jsfcrud.bean;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +15,9 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 import javax.faces.model.SelectItem;
+
+import org.primefaces.json.JSONObject;
+import org.primefaces.shaded.commons.io.IOUtils;
 
 import br.com.exercicio.jsfcrud.enumerator.Enderecos;
 import br.com.exercicio.jsfcrud.enumerator.Usuarios;
@@ -36,9 +43,85 @@ public class EnderecoBean {
 	@PostConstruct
 	public void initEstados() {
 		listEstado = new ArrayList<>();
-		listEstado.add(new SelectItem("PB", "Paraiba"));
+		listEstado.add(new SelectItem("AC", "Acre"));
+		listEstado.add(new SelectItem("AL", "Alagoas"));
+		listEstado.add(new SelectItem("AP", "Amapá"));
+		listEstado.add(new SelectItem("AM", "Amazonas"));
+		listEstado.add(new SelectItem("BA", "Bahia"));
+		listEstado.add(new SelectItem("CE", "Ceará"));
+		listEstado.add(new SelectItem("DF", "Distrito Federal"));
+		listEstado.add(new SelectItem("ES", "Espírito Santo"));
+		listEstado.add(new SelectItem("GO", "Goiás"));
+		listEstado.add(new SelectItem("MA", "Maranhão"));
+		listEstado.add(new SelectItem("MT", "Mato Grosso"));
+		listEstado.add(new SelectItem("MS", "Mato Grosso do Sul"));
+		listEstado.add(new SelectItem("MG", "Minas Gerais"));
+		listEstado.add(new SelectItem("PB", "Paraíba"));
+		listEstado.add(new SelectItem("PR", "Paraná"));
 		listEstado.add(new SelectItem("PE", "Pernambuco"));
+		listEstado.add(new SelectItem("RJ", "Rio de Janeiro"));
+		listEstado.add(new SelectItem("RN", "Rio Grande do Norte"));
+		listEstado.add(new SelectItem("RO", "Rondônia"));
+		listEstado.add(new SelectItem("RR", "Roraima"));
+		listEstado.add(new SelectItem("SC", "Santa Catarina"));
+		listEstado.add(new SelectItem("SP", "São Paulo"));
+		listEstado.add(new SelectItem("SE", "Sergipe"));
+		listEstado.add(new SelectItem("TO", "Tocantins"));		
+		
 	}
+	
+	public List<SelectItem> getListCidades(){
+		
+		List<SelectItem> listaCidades = new ArrayList<SelectItem>();
+		
+		try {	
+			String fileName = "estados-cidades.json";
+	        ClassLoader classLoader = new EnderecoBean().getClass().getClassLoader();	 
+	        File file = new File(classLoader.getResource(fileName).getFile());
+			
+	        if (file.exists()){			
+				
+			    InputStream is = new FileInputStream(file);
+					String jsonTxt = IOUtils.toString(is, "UTF-8");					
+					JSONObject json = new JSONObject(jsonTxt);						
+					
+					for (Object objEstado : json.getJSONArray("estados")) {
+						
+						JSONObject estado= new JSONObject(objEstado.toString());
+						//Buscando estado selecionado
+						
+						String uf="PB";
+						if(this.endereco.getEstado()!=null) {uf=this.endereco.getEstado();}
+						
+						if(estado.get("sigla").equals(uf.toUpperCase())) {
+							
+							for (Object objCidade : estado.getJSONArray("cidades")) {								
+								
+								System.out.println(objCidade.toString());
+								//JSONObject cidade = new JSONObject(objCidade.toString());								
+								
+								listaCidades.add(new SelectItem(objCidade.toString(), objCidade.toString()));
+								//listaCidades.add("","");
+								
+							}
+						}
+						
+						
+						
+					}
+			
+				return listaCidades;		
+				
+			   }
+			
+		}catch(Exception ex){
+			System.out.println("Erro" + ex.getMessage());
+		}		
+		 
+		return new ArrayList<SelectItem>();
+				
+	}
+	        
 	
 	public String prepararCadastro() {
 		endereco = new Endereco();
